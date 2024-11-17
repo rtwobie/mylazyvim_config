6a55b5b9f0a601e68171815f73969e87a4887f9a
unknown <robeats.hale@gmail.com>
2024-11-17 02:53:38 +0100

    new theme

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,22 +1,25 @@
 return {
+  -- Themes
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+  {
+    "xero/evangelion.nvim",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme("evangelion")
+    end,
+  },
+  { "nyngwang/nvimgelion" },
 
-	-- { "ellisonleao/gruvbox.nvim" },
-	-- {
-	-- 	"LazyVim/LazyVim",
-	-- 	opts = {
-	-- 		colorscheme = "gruvbox",
-	-- 	},
-	-- },
-	{
-		"xero/evangelion.nvim",
-		lazy = false,
-		priority = 1000,
-		init = function()
-			vim.cmd.colorscheme("evangelion")
-		end,
-	},
-	{ "mbbill/undotree" },
-	{ "ThePrimeagen/harpoon" },
-	{ "tpope/vim-fugitive" },
-	{ "ThePrimeagen/vim-be-good" },
+  -- Extensions
+  { "mbbill/undotree" },
+  { "ThePrimeagen/harpoon" },
+  { "tpope/vim-fugitive" },
+  { "ThePrimeagen/vim-be-good" },
 }
